708bbe9f262f3e0c23131d98ea933ba0db87bb67
Philip <okruzhko.f@gmail.com>
2023-10-04 23:26:10 +0400

    Complete s0m3t6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
+    public static void main(String[] args) {
+        FloatTypeCasting fc = new FloatTypeCasting();
+        fc.roundNumber(30.50f);
+    }
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
